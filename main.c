194893ce357b0bd5653b6a2f2a379cbfccad32fb
ENRIQUEMERCHAN <enrique.merchan@unet.edu.ve>
2016-08-11 10:35:16 -0400

    Add files via upload

---
+++ main.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <math.h>
+
+int main(void)
+{
+	int x1, x2, x3, x4, y1, y2, y3, y4;
+	
+	printf(" Ingrese los valores de los puntos para el mapa: ");
+	
+	printf("\n Ingrese x1 e y1: ");
+	scanf("%d %d",&x1,&y1);
+	printf("\n Ingrese x2 e y2: ");
+	scanf("%d %d",&x2,&y2);
+	printf("\n Ingrese x3 e y3: ");
+	scanf("%d %d",&x3,&y3);
+	printf("\n Ingrese x4 e y4: ");
+	scanf("%d %d",&x4,&y4);
+	
+	printf("\n\n Punto 1: (%d,%d)",x1,y1);
+	printf("\n Punto 2: (%d,%d)",x2,y2);
+	printf("\n punto 3: (%d,%d)",x3,y3);
+	printf("\n punto 4: (%d,%d)",x4,y4);
+	
+	l12=sqrt((x2))
+	return 0;
+}
